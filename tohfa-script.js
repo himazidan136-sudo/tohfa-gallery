7b8be0d8ba5aa7bdd677260ed75babea410a0f17
himazidan136-sudo <himazidan136@gmail.com>
2026-08-05 20:32:55 +0300

    C-220 DESKTOP-UHG43DP 2026-08-05 20:32

--- tohfa-script.js
+++ tohfa-script.js
@@ -296,7 +296,8 @@ const allProducts = [
     { name: "بلوره تاور", price: 400, img: "tohfa/decorb2.jpeg" },
     { name: " اباجوره سمارت", price:580 , img: "tohfa/abajora.jpg.jpeg" },
     { name: " اباجوره يقطينه مضيئه", price:845 , img: "tohfa/decora.jpeg" },
-    { name: "بوكس مناديل بابلز اسود", price:845 , img: "tohfa/decora.jpeg" },
+    { name: "بوكس مناديل بابلز اسود", price:400 , img: "tohfa/decor3b.jpeg" },
+    { name: "بوكس مناديل بابلز ابيض", price:500 , img: "tohfa/decor3w.jpeg" },
 
     // عشان تضيف منتج جديد.. خد السطر اللي فوق "نسخ" وغير البيانات بس
 ];
