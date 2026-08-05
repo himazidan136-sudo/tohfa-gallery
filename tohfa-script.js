88faae54a2cc78d240df43e38beae22d7a6ea6a2
himazidan136-sudo <himazidan136@gmail.com>
2026-08-05 20:25:57 +0300

    C-209 DESKTOP-UHG43DP 2026-08-05 20:25

--- tohfa-script.js
+++ tohfa-script.js
@@ -290,7 +290,7 @@ document.addEventListener('click', function(e) {
 const allProducts = [
     { name: "شمعدان ثلاثي ", price: 1495, img: "tohfa/sham3dan.jpg.jpeg" },
     { name: "مبخره هيدستينس", price: 600, img: "tohfa/mab5ara.jpg.jpeg" },
-    { name: "مبخره هيدستينس افريقيه", price: 630, img: "tohfa/mab5ara.jpg.jpeg" },
+    { name: "مبخره هيدستينس افريقيه", price: 630, img: "tohfa/decorm.jpg.jpeg" },
     { name: "ورد مجفف", price: 600, img: "tohfa/4.jpg.jpeg" },
     { name: "تمثال أنتيك", price: 2800, img: "tohfa/5.jpg.jpeg" },
     { name: "ساعة كلاسيك", price: 1100, img: "tohfa/9.jpg.jpeg" },
