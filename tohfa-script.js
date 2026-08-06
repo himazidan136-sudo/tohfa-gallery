f8c39da90cd6b84088b3ec9b1a9073c8c3196e48
himazidan136-sudo <himazidan136@gmail.com>
2026-08-06 18:27:15 +0300

    C-383 DESKTOP-UHG43DP 2026-08-06 18:27

--- tohfa-script.js
+++ tohfa-script.js
@@ -421,7 +421,10 @@ const vasesProducts = [
     { name: "مزهريه ذهبيه كبير", price: "750 ج.م", img: "tohfa/vasesm.jpeg" },
     { name: "مزهريه ذهبيه وسط", price: "620 ج.م", img: "tohfa/vasesmm.jpeg" },
     { name: "مزهريه ذهبيه صغير", price: "580 ج.م", img: "tohfa/vasesms.jpeg" },
-    
+    { name: "مزهريه خزفيه ذهبيه كبير", price: "580 ج.م", img: "tohfa/vasesmk.jpeg" },
+    { name: "مزهريه خزفيه ذهبيه وسط", price: "580 ج.م", img: "tohfa/vasesmk.jpeg" },
+    { name: "مزهريه خزفيه ذهبيه صغير", price: "580 ج.م", img: "tohfa/vasesmk.jpeg" },
+
 
 
 
