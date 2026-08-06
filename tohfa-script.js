3947d6346d8f14a5eff3e32bb80def8595dfe82c
himazidan136-sudo <himazidan136@gmail.com>
2026-08-06 20:45:37 +0300

    C-402 DESKTOP-UHG43DP 2026-08-06 20:45

--- tohfa-script.js
+++ tohfa-script.js
@@ -427,8 +427,8 @@ const vasesProducts = [
     { name: "فاز زور اسود ", price: "1000 ج.م", img: "tohfa/vasesz.jpeg" },
     { name: "مزهريه زجاجيه قرع ", price: "720 ج.م", img: "tohfa/vasesmz0.jpeg" },
     { name: "مزهريه زجاجيه قرع صغير", price: "580 ج.م", img: "tohfa/vasesmz.jpeg" },
-    { name: "مزهريه زجاجيه اسود صغير", price: "580 ج.م", img: "tohfa/vasesm1.jpeg" },
-    { name: "مزهريه زجاجيه اسود ", price: "580 ج.م", img: "tohfa/vasesmz.jpeg" },
+    { name: "مزهريه زجاجيه اسود صغير", price: "780 ج.م", img: "tohfa/vasesm1.jpeg" },
+    { name: "مزهريه زجاجيه اسود ", price: "850 ج.م", img: "tohfa/vasesmz2.jpeg" },
 
     { name: "مزهريه خزفي زيتي ", price: "599 ج.م", img: "tohfa/vasesk.jpeg" },
     { name: "فاز اسود ميرور ", price: "1650 ج.م", img: "tohfa/vasesb.jpeg" },
