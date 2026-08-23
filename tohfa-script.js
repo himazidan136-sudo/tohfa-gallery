4c9bcdb03f7688632e8dee32405e98a26aaaa5d9
himazidan136-sudo <himazidan136@gmail.com>
2026-08-23 16:37:08 +0300

    C-81 DESKTOP-UHG43DP 2026-08-23 16:37

--- tohfa-script.js
+++ tohfa-script.js
@@ -413,8 +413,10 @@ const flowerProducts = [
     { name: "بوت فايبر مع زرع شاميدورا", price: "2000 ج.م", img: "tohfa/b0.jpeg" },
     { name: "حوض مجاميع كبير ", price: "1550 ج.م", img: "tohfa/b1.jpeg" },
     { name: "حوض مجاميع صغير ", price: "1100 ج.م", img: "tohfa/b2.jpeg" },
-    { name: "بوت فايبر كوبايه مع زرع اريكا", price: "2200 ج.م", img: "tohfa/b3.jpeg" },
-    { name: "حوض مجاميع صغير ", price: "1100 ج.م", img: "tohfa/b4.jpeg" },
+    { name: "بوت فراوله مع زرع كولكاسيا", price: "480 ج.م", img: "tohfa/b3.jpeg" },
+    { name: "بوت كريستاله مع كرز ", price: "1100 ج.م", img: "tohfa/b4.jpeg" },
+
+
     { name: "صباره فستان استانلس", price: "300 ج.م", img: "tohfa/flowersf.jpeg" },
     { name: "صباره وش استانلس", price: "300 ج.م", img: "tohfa/flowersw.jpeg" },
     { name: "حامل نباتات استانلس", price: "200 ج.م", img: "tohfa/flowersh.jpeg" },
