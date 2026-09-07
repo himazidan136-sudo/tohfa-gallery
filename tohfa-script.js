6cf81c754601c5df6a190131b2b2adb46437067c
himazidan136-sudo <himazidan136@gmail.com>
2026-09-07 22:13:40 +0300

    C-64 DESKTOP-UHG43DP 2026-09-07 22:13

--- tohfa-script.js
+++ tohfa-script.js
@@ -348,9 +348,9 @@ const allProducts = [
 
     { name: "ديكور اب", price: 450, img: "tohfa/decor0.jpeg", stock: 1 },
     { name: "ديكور اب اريكه", price: 450, img: "tohfa/decor2.jpeg", stock: 1 },
-    { name: "بلوره تاور", price: 400, img: "tohfa/decor3.jpeg", stock: 1 },
-    { name: "بلوره تاور", price: 400, img: "tohfa/decor4.jpeg", stock: 1 },
-    { name: "بلوره تاور", price: 400, img: "tohfa/decor5.jpeg", stock: 1 },
+    { name: "بنت عوسه ابيض", price: 700, img: "tohfa/decor3.jpeg", stock: 1 },
+    { name: "بنت عوسه مع طبق", price: 750, img: "tohfa/decor4.jpeg", stock: 1 },
+    { name: "انتيك امراه دهبي", price: 705, img: "tohfa/decor5.jpeg", stock: 1 },
 
     { name: "بلوره مع حامل معدني", price: 300, img: "tohfa/decorb.jpeg", stock: 1 },
     { name: "بلوره مع حامل رخام", price: 300, img: "tohfa/decorb1.jpeg", stock: 1 },
