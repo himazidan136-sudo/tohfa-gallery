68d55c7c88c0fb22fbfc9049639a9bf339fa15c7
himazidan136-sudo <himazidan136@gmail.com>
2026-09-07 22:10:09 +0300

    C-61 DESKTOP-UHG43DP 2026-09-07 22:10

--- tohfa-script.js
+++ tohfa-script.js
@@ -345,7 +345,7 @@ const allProducts = [
     { name: "طقم شمعدان ميرور ورد ", price: 1250, img: "tohfa/sham3dan2.jpeg", stock: 2 },
     { name: "مبخره هيدستينس", price: 600, img: "tohfa/mab5ara.jpg.jpeg", stock: 1 },
     { name: "مبخره هيدستينس افريقيه", price: 630, img: "tohfa/decorm.jpeg", stock: 1 },
-        { name: "بلوره تاور", price: 400, img: "tohfa/decorb2.jpeg", stock: 1 },
+    { name: "بلوره تاور", price: 400, img: "tohfa/decorb2.jpeg", stock: 1 },
     { name: "بلوره تاور", price: 400, img: "tohfa/decorb2.jpeg", stock: 1 },
     { name: "بلوره تاور", price: 400, img: "tohfa/decorb2.jpeg", stock: 1 },
 
