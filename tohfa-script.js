81c2cf05ddc5888222277a3d3558b4e968c0554b
himazidan136-sudo <himazidan136@gmail.com>
2026-09-07 21:56:49 +0300

    C-42 DESKTOP-UHG43DP 2026-09-07 21:56

--- tohfa-script.js
+++ tohfa-script.js
@@ -331,6 +331,12 @@ function isAvailable(item) {
 
 // 1. مخزن المنتجات (ضيف هنا كل المنتجات اللي معاك في ثواني)
 const allProducts = [
+    { name: "شمعدان ثلاثي ", price: 1495, img: "tohfa/sham3dan3.jpg.jpeg", stock: 1 },
+    { name: "شمعدان ثلاثي ", price: 1495, img: "tohfa/sham3dan4.jpg.jpeg", stock: 1 },
+    { name: "شمعدان ثلاثي ", price: 1495, img: "tohfa/sham3dan5.jpg.jpeg", stock: 1 },
+    { name: "شمعدان ثلاثي ", price: 1495, img: "tohfa/sham3dan6.jpg.jpeg", stock: 1 },
+    { name: "شمعدان ثلاثي ", price: 1495, img: "tohfa/sham3dan7.jpg.jpeg", stock: 1 },
+
     { name: "شمعدان ثلاثي ", price: 1495, img: "tohfa/sham3dan.jpg.jpeg", stock: 1 },
     { name: "طقم شمعدان ميرور حلقات ", price: 1100, img: "tohfa/sham3dan1.jpeg", stock: 2 },
     { name: "طقم شمعدان ميرور ورد ", price: 1250, img: "tohfa/sham3dan2.jpeg", stock: 2 },
