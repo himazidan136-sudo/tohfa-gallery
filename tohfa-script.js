f3efc75b022d3a090b72c97f4523d4cb040b5104
himazidan136-sudo <himazidan136@gmail.com>
2026-09-07 21:58:53 +0300

    C-44 DESKTOP-UHG43DP 2026-09-07 21:58

--- tohfa-script.js
+++ tohfa-script.js
@@ -331,7 +331,7 @@ function isAvailable(item) {
 
 // 1. مخزن المنتجات (ضيف هنا كل المنتجات اللي معاك في ثواني)
 const allProducts = [
-    { name: "شمعدان ثلاثي ", price: 1495, img: "tohfa/sham3dan3.jpeg", stock: 1 },
+    { name: " طقم شمعدان اسود ", price: 1495, img: "tohfa/sham3dan3.jpeg", stock: 1 },
     { name: "شمعدان ثلاثي ", price: 1495, img: "tohfa/sham3dan4.jpeg", stock: 1 },
     { name: "شمعدان ثلاثي ", price: 1495, img: "tohfa/sham3dan5.jpeg", stock: 1 },
     { name: "شمعدان ثلاثي ", price: 1495, img: "tohfa/sham3dan6.jpeg", stock: 1 },
