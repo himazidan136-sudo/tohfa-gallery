f1e207ab4a9bee3762158a08cccbefa3e8809759
himazidan136-sudo <himazidan136@gmail.com>
2026-09-08 19:45:44 +0300

    C-114 DESKTOP-UHG43DP 2026-09-08 19:45

--- tohfa-script.js
+++ tohfa-script.js
@@ -380,8 +380,8 @@ const allProducts = [
     { name: "مرايا بابلز  ", price:250 , img: "tohfa/dm.jpeg", stock: 1 },
     { name: "مرايا كريستال  ", price:490 , img: "tohfa/dm0.jpeg", stock: 1 },
     { name: "فواحه   ", price:490 , img: "tohfa/df.jpeg", stock: 1 },
-    { name: "فواحه اطفال ", price:490 , img: "tohfa/df0.jpeg", stock: 1 },
-    { name: "فواحه   ", price:490 , img: "tohfa/df1.jpeg", stock: 1 },
+    { name: "فواحه اطفال ", price:320 , img: "tohfa/df0.jpeg", stock: 1 },
+    { name: "فواحه اطفال قطه  ", price:230 , img: "tohfa/df1.jpeg", stock: 1 },
 
     { name: "صينيه تقديم  ", price:350 , img: "tohfa/shayala.jpeg", stock: 1 },
     { name: "منظم ابيض  ", price:1100 , img: "tohfa/shayala1.jpeg", stock: 1 },
