35f5400efa7ffa09a1ce301010b75f697d06bec0
himazidan136-sudo <himazidan136@gmail.com>
2026-09-08 21:03:20 +0300

    C-154 DESKTOP-UHG43DP 2026-09-08 21:03

--- tohfa-script.js
+++ tohfa-script.js
@@ -633,8 +633,8 @@ const wallProducts = [
        { name: "برواز خشب 40*30", price: "200 ج.م", img: "tohfa/wall/t14.jpeg", stock: 1 },
        { name: "", price: "850 ج.م", img: "tohfa/wall/t15.jpeg", stock: 1 },
        { name: "", price: "850 ج.م", img: "tohfa/wall/t16.jpeg", stock: 1 },
-       { name: "", price: "850 ج.م", img: "tohfa/wall/t17.jpeg", stock: 1 },
-       { name: "", price: "850 ج.م", img: "tohfa/wall/t18.jpeg", stock: 1 },
+       { name: "برواز خشب 40*30", price: "200 ج.م", img: "tohfa/wall/t17.jpeg", stock: 1 },
+       { name: "برواز خشب 40*30", price: "200 ج.م", img: "tohfa/wall/t18.jpeg", stock: 1 },
 
 
 
