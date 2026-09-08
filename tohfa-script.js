0266df7d0b75c5881ec1d413e1cdef50850e7f14
himazidan136-sudo <himazidan136@gmail.com>
2026-09-08 20:08:02 +0300

    C-127 DESKTOP-UHG43DP 2026-09-08 20:08

--- tohfa-script.js
+++ tohfa-script.js
@@ -465,7 +465,7 @@ const flowerProducts = [
     { name: "فرع كريستال مستورد  ", price: "90 ج.م", img: "tohfa/z/z14.jpeg", stock: 1 },
 
     { name: " مقزمه معدن مضيئه ابيض ", price: "520 ج.م", img: "tohfa/z/f.jpeg", stock: 1 },
-    { name: "  مقزمه معدن مضي", price: "300 ج.م", img: "tohfa/z/f0.jpeg", stock: 1 },
+    { name: "  مقزمه معدن مضيئه اسود", price: "520 ج.م", img: "tohfa/z/f0.jpeg", stock: 1 },
     { name: "  ", price: "300 ج.م", img: "tohfa/z/f1.jpeg", stock: 1 },
     { name: "  ", price: "300 ج.م", img: "tohfa/z/f2.jpeg", stock: 1 },
     { name: "  ", price: "300 ج.م", img: "tohfa/z/f3.jpeg", stock: 1 },
