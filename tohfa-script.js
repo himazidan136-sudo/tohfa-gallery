5d00e5cd0f3e09c468444bbb519fb1ee5e75456c
himazidan136-sudo <himazidan136@gmail.com>
2026-09-08 20:57:53 +0300

    C-148 DESKTOP-UHG43DP 2026-09-08 20:57

--- tohfa-script.js
+++ tohfa-script.js
@@ -621,9 +621,9 @@ const wallProducts = [
        { name: "برواز ثلاثي 40*60 مودرن مخطط", price: "900ج.م", img: "tohfa/wall/t3.jpeg", stock: 1 },
        { name: "برواز ثلاثي 40*60 قران سور", price: "900ج.م", img: "tohfa/wall/t4.jpeg", stock: 1 },
        { name: "برواز ثلاثي 40*60 قران ", price: "900ج.م", img: "tohfa/wall/t5.jpeg", stock: 1 },
-       { name: "برواز ثلاثي 40*60", price: "500.م", img: "tohfa/wall/t6.jpeg", stock: 1 },
        { name: "برواز ثلاثي 40*60 اطفال قران", price: "900ج.م", img: "tohfa/wall/t7.jpeg", stock: 1 },
        { name: "برواز ثلاثي 40*60 موردن بيج", price: "900 ج.م", img: "tohfa/wall/t8.jpeg", stock: 1 },
+       { name: "برواز ثلاثي  ", price: "500.م", img: "tohfa/wall/t6.jpeg", stock: 1 },
 
        { name: "", price: "850 ج.م", img: "tohfa/wall/t9.jpeg", stock: 1 },
        { name: "", price: "850 ج.م", img: "tohfa/wall/t10.jpeg", stock: 1 },
