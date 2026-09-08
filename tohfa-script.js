8ee68cae1d49554489aa73f6339f1ff394bc1dca
himazidan136-sudo <himazidan136@gmail.com>
2026-09-08 20:46:28 +0300

    C-134 DESKTOP-UHG43DP 2026-09-08 20:46

--- tohfa-script.js
+++ tohfa-script.js
@@ -613,7 +613,37 @@ const wallProducts = [
     { name: "تابلوه جلد بحار 120*80", price: "1200 ج.م", img: "tohfa/wall/w10.jpeg", stock: 1 },
     { name: "تابلوه جلد  90*50", price: "850 ج.م", img: "tohfa/wall/w11.jpeg", stock: 1 },
     { name: "تابلوه جلد 80*60", price: "850 ج.م", img: "tohfa/wall/w13.jpeg", stock: 1 },
-   
+    { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+       { name: "", price: "850 ج.م", img: "tohfa/wall/t.jpeg", stock: 1 },
+
+
+
+
+
+
+
+
+
+    
 ];
 
 // 2. وظيفة عرض الورد تلقائياً
