eb71e3bdb64a9f29508095bd9d50ecda5a86246a
himazidan136-sudo <himazidan136@gmail.com>
2026-09-08 19:50:40 +0300

    C-118 DESKTOP-UHG43DP 2026-09-08 19:50

--- tohfa-script.js
+++ tohfa-script.js
@@ -443,8 +443,8 @@ const flowerProducts = [
     { name: "بوت فراوله مع زرع كولكاسيا", price: "480 ج.م", img: "tohfa/z/b3.jpeg", stock: 1 },
     { name: "بوت كريستاله مع كريز ", price: "1120 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
 
-    { name: "فرع عباد شمس مستورد  ", price: "295 ج.م", img: "tohfa/z/z15.jpeg", stock: 1 },
-    { name: "فرع عباد شمس مستورد  ", price: "295 ج.م", img: "tohfa/z/z16.jpeg", stock: 1 },
+    { name: "لوزه 9 ورده مستورد  ", price: "210 ج.م", img: "tohfa/z/z15.jpeg", stock: 1 },
+    { name: "مجموعه عباد شمس مستورد  ", price: "295 ج.م", img: "tohfa/z/z16.jpeg", stock: 1 },
     { name: "فرع عباد شمس مستورد  ", price: "295 ج.م", img: "tohfa/z/z17.jpeg", stock: 1 },
     { name: "فرع عباد شمس مستورد  ", price: "295 ج.م", img: "tohfa/z/z18.jpeg", stock: 1 },
     { name: "فرع عباد شمس مستورد  ", price: "295 ج.م", img: "tohfa/z/z19.jpeg", stock: 1 },
