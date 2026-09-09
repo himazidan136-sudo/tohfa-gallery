82120278a6c7b68713ddd6b868300ebe67fcf12d
himazidan136-sudo <himazidan136@gmail.com>
2026-09-09 17:32:29 +0300

    C-32 DESKTOP-UHG43DP 2026-09-09 17:32

--- tohfa-script.js
+++ tohfa-script.js
@@ -441,14 +441,14 @@ const flowerProducts = [
     { name: "حوض + مجاميع كبير ", price: "1550 ج.م", img: "tohfa/z/b1.jpeg", stock: 1 },
     { name: "حوض + مجاميع صغير ", price: "1100 ج.م", img: "tohfa/z/b2.jpeg", stock: 1 },
     { name: "بوت فراوله مع زرع كولكاسيا", price: "480 ج.م", img: "tohfa/z/b3.jpeg", stock: 1 },
+    
     { name: "بوت كريستاله مع كريز ", price: "1120 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
-
+    { name: "قرنفل 12 ورده مستورد  ", price: "250 ج.م", img: "tohfa/z/z20.jpeg", stock: 1 },
     { name: "لوزه 9 ورده مستورد  ", price: "210 ج.م", img: "tohfa/z/z15.jpeg", stock: 1 },
     { name: "مجموعه ورد احمر قطيفه مستورد  ", price: "210 ج.م", img: "tohfa/z/z16.jpeg", stock: 1 },
     { name: "براعم 2 لون مستورد  ", price: "150 ج.م", img: "tohfa/z/z17.jpeg", stock: 1 },
     { name: "براعم 5 ورده بينك مستورد  ", price: "110 ج.م", img: "tohfa/z/z18.jpeg", stock: 1 },
     { name: "براعم 5 ورده لبني مستورد  ", price: "110 ج.م", img: "tohfa/z/z19.jpeg", stock: 1 },
-    { name: "قرنفل 12 ورده مستورد  ", price: "250 ج.م", img: "tohfa/z/z20.jpeg", stock: 1 },
     { name: "براعم 5 ورده بنفسجي مستورد  ", price: "110 ج.م", img: "tohfa/z/z21.jpeg", stock: 1 },
     { name: "براعم 5 ورده وردي مستورد  ", price: "110 ج.م", img: "tohfa/z/z22.jpeg", stock: 1 },
 
