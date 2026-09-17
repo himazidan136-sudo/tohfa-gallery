8267f3f1f7f91400f635eec92118c291b4af821b
himazidan136-sudo <himazidan136@gmail.com>
2026-09-17 15:54:38 +0300

    C-43 DESKTOP-UHG43DP 2026-09-17 15:54

--- tohfa-script.js
+++ tohfa-script.js
@@ -613,8 +613,8 @@ const wallProducts = [
     { name: "تابلوه هاند ميد ورد بارز اخضر  120*80 ", price: "2150 ج.م", img: "tohfa/wall/w8.jpeg", stock: 1 },
     { name: "تابلوه جلد بنت افريقيه 120*80", price: "1200 ج.م", img: "tohfa/wall/w9.jpeg", stock: 1 },
     { name: "تابلوه جلد بحار 120*80", price: "1200 ج.م", img: "tohfa/wall/w10.jpeg", stock: 1 },
-    
-    { name: "تابلوه جلد  90*50", price: "850 ج.م", img: "tohfa/wall/w11.jpeg", stock: 1 },
+
+    { name: "تابلوه مدخل مضئ  90*50", price: "850 ج.م", img: "tohfa/wall/w11.jpeg", stock: 1 },
     { name: "تابلوه جلد 80*60", price: "850 ج.م", img: "tohfa/wall/w13.jpeg", stock: 1 },
     { name: "تابلوه جلد 80*60", price: "850 ج.م", img: "tohfa/wall/w13.jpeg", stock: 1 },
     { name: "تابلوه جلد 80*60", price: "850 ج.م", img: "tohfa/wall/w13.jpeg", stock: 1 },
