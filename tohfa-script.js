bebfb1e81d23fd5a2995c52a8706c569b75c1017
himazidan136-sudo <himazidan136@gmail.com>
2026-09-17 16:01:30 +0300

    C-50 DESKTOP-UHG43DP 2026-09-17 16:01

--- tohfa-script.js
+++ tohfa-script.js
@@ -436,7 +436,7 @@ document.addEventListener('DOMContentLoaded', renderProducts);
 
 // 1. مخزن بيانات الورد (كل منتج في سطر واحد زي ما طلبت)
 const flowerProducts = [
-    { name: "بوت كريستاله مع كريز ", price: "1120 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
+    { name: "بوت فايبر كبير مع زرع عصفوره الجنه ", price: "1120 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
     { name: "بوت كريستاله مع كريز ", price: "1120 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
     { name: "بوت كريستاله مع كريز ", price: "1120 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
     { name: "بوت كريستاله مع كريز ", price: "1120 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
