cc057fe53e12dff6fd6ea0b7aea0be65d2078d95
himazidan136-sudo <himazidan136@gmail.com>
2026-09-17 16:03:33 +0300

    C-55 DESKTOP-UHG43DP 2026-09-17 16:03

--- tohfa-script.js
+++ tohfa-script.js
@@ -438,7 +438,7 @@ document.addEventListener('DOMContentLoaded', renderProducts);
 const flowerProducts = [
     { name: "بوت فايبر كبير مع زرع عصفوره الجنه ", price: "4750 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
     { name: "بوت فيونكه مع زرع اريكا ", price: "680 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
-    { name: "بوت صاروخ مع كريز ", price: "1120 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
+    { name: "بوت صاروخ مع موز ", price: "1120 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
     { name: "بوت كريستاله مع كريز ", price: "1120 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
     { name: "بوت كريستاله مع كريز ", price: "1120 ج.م", img: "tohfa/z/b4.jpeg", stock: 1 },
 
