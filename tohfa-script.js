9e0aafab9d89ffe6b80513a395fc0d27bac69daa
himazidan136-sudo <himazidan136@gmail.com>
2026-09-22 15:10:11 +0300

    C-84 DESKTOP-UHG43DP 2026-09-22 15:10

--- tohfa-script.js
+++ tohfa-script.js
@@ -351,7 +351,7 @@ const allProducts = [
     { name: " طقم طرابيزه 3 قطع اسود", price: 1550, img: "tohfa/tb.jpeg", stock: 1 },
     { name: " طقم طرابيزه 3 قطع ازرق", price: 1550, img: "tohfa/tb.jpeg", stock: 1 },
     { name: " طقم طرابيزه قطعتين لبني", price: 1650, img: "tohfa/tw.jpeg", stock: 1 },
-    { name: "طرابيزه اسود", price: 450, img: "tohfa/tb0.jpeg", stock: 1 },
+    { name: "طرابيزه اسود", price: 650, img: "tohfa/tb0.jpeg", stock: 1 },
 
     { name: "ديكور اب", price: 450, img: "tohfa/decor0.jpeg", stock: 1 },
     { name: "ديكور اب اريكه", price: 450, img: "tohfa/decor2.jpeg", stock: 1 },
